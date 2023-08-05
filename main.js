570321c00bf0665a0c6122b868f97f7d8042f103
rauakmpct <rkloveak0118@gmail.com>
2023-08-05 12:21:08 +0530

    done

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let secItem = document.querySelector("#sec");
 
 
 let countDown = () => {
-  let futureDate = new Date("1 Jan 2024");
+  let futureDate = new Date("15 August 2023");
   let currentDate = new Date();
   let myDate = futureDate - currentDate;
   //console.log(myDate);
